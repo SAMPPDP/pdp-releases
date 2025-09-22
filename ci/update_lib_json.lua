3f1d5772f07f76dab6583a09e4004a38e460dd3a
Mikhail Potemkin <63658002+kyrtion@users.noreply.github.com>
2025-09-22 19:45:07 +1000

    ci: again test pls

--- ci/update_lib_json.lua
+++ ci/update_lib_json.lua
@@ -6,41 +6,43 @@ local sha1 = require('sha1')
 -- local path_lib = path_github .. '/lua/lib'
 
 local function scan_directory(path)
-  local directory_iterator, error_message = lfs.dir(path)
+  local file_attributes = lfs.attributes(path)
 
-  if not directory_iterator then
-    error('Error opening directory: ' .. path .. ' - ' .. error_message)
+  if not file_attributes then
+    print('Error getting attributes for: ' .. path)
+    return
   end
 
-  for entry in directory_iterator do
-    if entry ~= '.' and entry ~= '..' then
-      local full_path = path .. '/' .. entry
-      local file_attributes = lfs.attributes(full_path)
+  if file_attributes.mode == 'file' then
+    print('File: ' .. path)
+    -- print('  -- заметка: можно применить file = io.open(' .. path .. ', 'r') чтобы читать файл')
+    local file, err = io.open(path)
+    if not file or err then
+      error(path .. '(type: ' .. type(file) .. ') -> Error: ' .. err)
+    end
+    return -- Stop scanning if it's a file
+  elseif file_attributes.mode == 'directory' then
+    local directory_iterator = lfs.dir(path)
 
-      if file_attributes then
-        if file_attributes.mode == 'file' then
-          local file, errmsg = io.open(full_path)
-          if not file or errmsg then
-            error(errmsg)
-          end
-          local source = file:read('a')
-          file:close()
-          print('File: ' .. full_path)
-          print('SHA1.SHA1: ' .. sha1.sha1(source))
-          print('SHA1 Binary: ' .. sha1.binary(source))
-        elseif file_attributes.mode == 'directory' then
-          print('Directory: ' .. full_path)
-          scan_directory(full_path) -- recursive sub-dir
-        else
-          print('Unknown entry: ' .. full_path .. ' (mode: ' .. file_attributes.mode .. ')')
-        end
-      else
-        print('Error getting attributes for: ' .. full_path)
+    if type(directory_iterator) ~= 'function' then
+      print('Error opening directory: ' .. path)
+      return
+    end
+
+    for entry in directory_iterator do
+      if entry ~= '.' and entry ~= '..' then
+        local full_path = path .. '/' .. entry
+        scan_directory(full_path) -- Recursive call
       end
     end
+  else
+    print('Unknown entry: ' .. path .. ' (mode: ' .. file_attributes.mode .. ')')
   end
 end
 
-scan_directory(lfs.currentdir()) -- Start scanning from the current directory
+local current_dir = lfs.currentdir()
+print(current_dir)
+
+scan_directory(current_dir) -- Start scanning from the current directory
 
 -- local str_json = dkjson.encode(data, { indent = 4 })
